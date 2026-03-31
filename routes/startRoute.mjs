741c5a94c117cafeae4f1121e2047ecf914b1c03
John Schmidt <John.Schmidt@mubea.com>
2026-03-31 12:00:39 +0200

    Reupload all files as fresh commit

--- routes/startRoute.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { getStart } from '../controllers/viewsController.mjs';
-
-const router = express.Router();
-
-router.get('/', getStart);
-
-export default router;
